4808956147f9fd1e5d6b072fcb21bd3a22b1d93b
Muhammad Zohaib Hassan <56926961+user-zoobi@users.noreply.github.com>
2022-05-24 08:52:26 +0500

    Update MainActivity.kt

--- Android Examples/PopupMenu/kt/MainActivity.kt
+++ Android Examples/PopupMenu/kt/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.androidexamples
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.view.View
 import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -11,6 +14,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private lateinit var context:Context
+    private lateinit var view:View
 
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,21 +23,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.button.setOnClickListener {
-            val popMenu = PopupMenu(this, button)
-            popMenu.menuInflater.inflate(R.menu.popup_menu, popMenu.menu)
-            popMenu.show()
-            popMenu.setForceShowIcon(true)
-
-            popMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
-                when(item.itemId){
-                    R.id.home-> Toast.makeText(this, "home", Toast.LENGTH_SHORT).show()
-                    R.id.settings-> Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
-                    R.id.share-> Toast.makeText(this, "share", Toast.LENGTH_SHORT).show()
-                }
-                true
-            })
-
+            onMenuItemClick(this)
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun onMenuItemClick(context: Context){
+        val wrapper: Context =  ContextThemeWrapper(context, R.style.CustomPopUpStyle)
+        val popMenu = PopupMenu(wrapper, button)
+        popMenu.menuInflater.inflate(R.menu.popup_menu, popMenu.menu)
+        popMenu.show()
+        popMenu.setForceShowIcon(true)
+
+        popMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+            when(item.itemId){
+                R.id.home-> Toast.makeText(this, "home", Toast.LENGTH_SHORT).show()
+                R.id.settings-> Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+                R.id.share-> Toast.makeText(this, "share", Toast.LENGTH_SHORT).show()
+            }
+            true
+        })
     }
 
 }
